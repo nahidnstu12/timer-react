948a9693d4c18fa1ccf76dda34f097a8f863e184
= <=>
2020-07-24 07:48:48 +0600

    Folder restructure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+// import Todo from './todoList';
+// import Timer from './timer';
+// import Game from './TicTackToe/game'
+import StackTodos from './todoStack/app'
+
+library.add(faTrash)
+
+class App extends Component{
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     {/* <Timer/> */}
+    
+     {/* <Todo/> */}
+
+     {/* <Game /> */}
+
+     <StackTodos />
+
     </div>
-  );
+  )
+ }
 }
 
 export default App;
